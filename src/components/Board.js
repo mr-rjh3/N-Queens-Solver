41aec9e98e3d1d1b22be42ad3aca8e2d4265d780
mr-rjh3 <mrrjh3@gmail.com>
2022-11-17 11:04:22 -0500

    oops didnt stage all changes

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,43 +1,53 @@
-import React from "react"; // ES6 js
+import React, {useState} from "react"; // ES6 js
+function Board () { // This is a class that extends the React.Component class
+    // This returns HTML code
 
-function Square(props) {
-    return (
-        <div className="square" style={{backgroundColor: props.backgroundColor}}/>
-    );
-}
-
-// arrow func to make board
-const createBoard = () => {
-    const board = [];
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            if(i % 2 === 0) {
-                if((j % 2 === 0)) {
-                    board.push(<Square backgroundColor= "var(--white)"/>);
+    
+    
+    
+    function Square(props) {
+        const [isActive, setIsActive] = useState(false);
+        const clickHandler = () => {
+            console.log("clicked");
+            setIsActive(current => !current);
+        }
+        return (
+            <button className="square"  style={{backgroundColor: props.backgroundColor,}} 
+                                        onClick={clickHandler}> 
+                <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg" style={{opacity: isActive ? '1' : '0'}}/>                           
+            </button>
+        );
+    }
+    
+    
+    // arrow func to make board
+    const createBoard = () => {
+        const board = [];
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if(i % 2 === 0) {
+                    if((j % 2 === 0)) {
+                        board.push(<Square backgroundColor= "var(--white)"/>);
+                    } else {
+                        board.push(<Square backgroundColor= "var(--black)"/>);
+                    }
                 } else {
-                    board.push(<Square backgroundColor= "var(--black)"/>);
-                }
-            } else {
-                if((j % 2 === 0)) {
-                    board.push(<Square backgroundColor= "var(--black)"/>);
-                } else {
-                    board.push(<Square backgroundColor= "var(--white)"/>);
+                    if((j % 2 === 0)) {
+                        board.push(<Square backgroundColor= "var(--black)"/>);
+                    } else {
+                        board.push(<Square backgroundColor= "var(--white)"/>);
+                    }
                 }
             }
         }
+        return board;
     }
-    return board;
-}
 
-class Board extends React.Component { // This is a class that extends the React.Component class
-    // This returns HTML code
-    render(){
-        return (
-            <div className="board">
-                {createBoard()}
-            </div>
-        );
-    }
+    return (
+        <div className="board">
+            {createBoard()}
+        </div>
+    );
 }
 
 export default Board;
