28a1d3558a414feea92fd6ac4564c620374a3357
mr-rjh3 <mrrjh3@gmail.com>
2022-11-18 20:25:44 -0500

    Implemented most of the frontend

--- src/App.js
+++ src/App.js
@@ -2,10 +2,64 @@
 import './App.css';
 import Board from './components/Board';
 
+const BOARD_SIZE = 8;
+
 function App() { // This returns HTML code
+        const findQueens = () => {
+          // Find all the queens
+          const collection = document.getElementsByClassName("hasQueen");
+          for (let i = 0; i < collection.length; i++) {
+            console.log("FOUND QUEEN AT: ", collection[i].id);
+            collection[i].firstChild.style.opacity = "0.8";
+          }
+        }
+        const resetBoard = () => {
+          // refresh the board
+          const collection = document.getElementsByClassName("hasQueen");
+          for (let i = 0; i < collection.length; i++) {
+            console.log("REMOVING QUEEN: ", collection[i].id);
+            collection[i].click();
+          }
+        }
+        const randPositions = (event) => {
+          // randomize the positions of the queens
+          const queens = document.getElementsByClassName("input")[0].value;
+          console.log(queens);
+          if(queens !== "" && queens <= (BOARD_SIZE * BOARD_SIZE)) {
+            resetBoard();
+            const squares = document.getElementsByClassName("square");
+            const numSet = new Set();
+            while(numSet.size < queens) {
+              numSet.add(Math.floor(Math.random() * (BOARD_SIZE * BOARD_SIZE)));
+            }
+            const nums = Array.from(numSet);
+            // console.log(nums.length);
+            for (let i = 0; i < nums.length; i++) {
+                console.log("RANDS: ", nums[i]);
+                squares[nums[i]].click();
+            }
+          }
+        }
+      /*
+      VISUALIZATION OF BOARD COORDS
+        A8 B8 C8 D8 E8 F8 G8 H8
+        A7 B7 C7 D7 E7 F7 G7 H7
+        A6 B6 C6 D6 E6 F6 G6 H6
+        A5 B5 C5 D5 E5 F5 G5 H5
+        A4 B4 C4 D4 E4 F4 G4 H4
+        A3 B3 C3 D3 E3 F3 G3 H3
+        A2 B2 C2 D2 E2 F2 G2 H2
+        A1 B1 C1 D1 E1 F1 G1 H1
+      */
+            
   return (
     <div className="App">
-      <button className="button">Start Game</button>
+      <header className="App-header">
+        <button className="button" onClick={resetBoard}>Reset Board</button>
+        <button className="button" onClick={findQueens}>Solve!</button>
+        <input className='input' type="number" placeholder="# of Queens" title="Enter number of queens to randomize." required/>
+        <button className="button" onClick={randPositions}>Randomize</button>
+      </header>
       <Board />
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,20 +1,34 @@
 import React, {useState} from "react"; // ES6 js
+
+const BOARD_SIZE = 8;
+
 function Board () { // This is a class that extends the React.Component class
     // This returns HTML code
-
-    
-    
-    
+ 
     function Square(props) {
         const [isActive, setIsActive] = useState(false);
-        const clickHandler = () => {
-            console.log("clicked");
+        const squareClick = (event) => {
+            let highlight;
+            // console.log(event.target);
+            if(event.target.className === "square " || event.target.className === "square hasQueen") {
+                highlight = event.target.childNodes[0];
+            }
+            if(event.target.className === "image") {
+                highlight = event.target.parentNode.childNodes[0];
+            }
+            // console.log(highlight);
+                
+            // If the square is already red, make it not that
+            if(highlight.style.opacity !== "0") {
+                highlight.style.opacity = "0";
+            }
             setIsActive(current => !current);
         }
         return (
-            <button className="square"  style={{backgroundColor: props.backgroundColor,}} 
-                                        onClick={clickHandler}> 
-                <img src="https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg" style={{opacity: isActive ? '1' : '0'}}/>                           
+            <button id = {props.id} className={`square ${isActive ? 'hasQueen' : ''}`}  style={{backgroundColor: props.backgroundColor,}} 
+                                        onClick={squareClick}> 
+                <div className="highlight"/>
+                <img className="image" src="https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg" style={{opacity: isActive ? '1' : '0'}}/>
             </button>
         );
     }
@@ -23,21 +37,23 @@ function Board () { // This is a class that extends the React.Component class
     // arrow func to make board
     const createBoard = () => {
         const board = [];
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
+        let count = 0;
+        for (let i = 0; i < BOARD_SIZE; i++) {
+            for (let j = 0; j < BOARD_SIZE; j++) {
                 if(i % 2 === 0) {
                     if((j % 2 === 0)) {
-                        board.push(<Square backgroundColor= "var(--white)"/>);
+                        board.push(<Square id={count} backgroundColor="var(--white)"/>);
                     } else {
-                        board.push(<Square backgroundColor= "var(--black)"/>);
+                        board.push(<Square id={count} backgroundColor="var(--black)"/>);
                     }
                 } else {
                     if((j % 2 === 0)) {
-                        board.push(<Square backgroundColor= "var(--black)"/>);
+                        board.push(<Square id={count} backgroundColor="var(--black)"/>);
                     } else {
-                        board.push(<Square backgroundColor= "var(--white)"/>);
+                        board.push(<Square id={count} backgroundColor="var(--white)"/>);
                     }
                 }
+                count++;
             }
         }
         return board;
